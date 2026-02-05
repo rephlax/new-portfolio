135600f86a5f78c0700acdb4109c20b425b9951d
Luke Farrell <lukefarrell2012@gmail.com>
2026-02-05 23:37:16 +0000

    Tablist context setup

---
+++ src/context/TabListProvider.tsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+type TabListContextProviderProps = {
+	children: React.ReactNode;
+};
+
+type TabListContextType = {
+	openWindows: string[];
+	minimisedWindows: string[];
+	minimise: (windowName: string) => void;
+	restore: (windowName: string) => void;
+};
+
+export const Context = createContext({} as TabListContextType);
+
+export const ThemeContextProvider = ({
+	children,
+}: TabListContextProviderProps) => {
+	const [openWindows, setOpenWindows] = useState<string[]>([
+		"About",
+		"Finder",
+		"LightHouse",
+	]);
+	const [minimisedWindows, setMinimisedWindows] = useState<string[]>([]);
+
+	const minimise = (windowName: string) => {
+		setMinimisedWindows((prev) => [...prev, windowName]);
+		setOpenWindows((prev) => prev.filter((name) => name !== windowName));
+	};
+	const restore = (windowName: string) => {
+		setOpenWindows((prev) => [...prev, windowName]);
+		setMinimisedWindows((prev) => prev.filter((name) => name !== windowName));
+	};
+	return (
+		<Context.Provider
+			value={{ openWindows, minimisedWindows, minimise, restore }}
+		>
+			{children}
+		</Context.Provider>
+	);
+};
