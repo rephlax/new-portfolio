d72865f2c7f20eb4ba442ebe6ec68a9bff33009c
Luke Farrell <lukefarrell2012@gmail.com>
2026-02-17 18:59:52 +0000

    A little bit more of a filling out of the finder

--- src/components/MacOS/FinderWindow/FinderWindow.tsx
+++ src/components/MacOS/FinderWindow/FinderWindow.tsx
@@ -18,6 +18,10 @@ const FinderWindow: FC<FinderWindowProps> = ({ containerRef }) => (
 				<a>Contact Me</a>
 			</div>
 			<div id="content">
+				<div id="default-window">
+					<img />
+					<p>Hi! I'm Luke</p>
+				</div>
 				{/* ADD A CONDITIONAL THAT WILL RENDER THE DIFFERENT SCREENS IF THE BUTTON IS PUSHED, IF NOT SHOW A PICTURE OF ME WITH A HEADLINE OF MYSELF */}
 			</div>
 		</section>
