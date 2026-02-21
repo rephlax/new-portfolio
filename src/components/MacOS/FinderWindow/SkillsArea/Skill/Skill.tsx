776333edb71257cb165c2d8d5b5bfb8100cf9da2
Luke Farrell <lukefarrell2012@gmail.com>
2026-02-21 00:01:58 +0000

    Working on the skeleton of the skill child comp

--- src/components/MacOS/FinderWindow/SkillsArea/Skill/Skill.tsx
+++ src/components/MacOS/FinderWindow/SkillsArea/Skill/Skill.tsx
@@ -1,5 +1,6 @@
 import React, { FC, type ReactNode } from "react";
 import styles from "./Skill.module.css";
+import { motion } from "motion/react";
 
 interface SkillProps {
 	children?: ReactNode;
@@ -12,6 +13,21 @@ const Skill: FC<SkillProps> = ({
 	skillName,
 	loadingBarPercentage,
 	category,
-}) => <div className={styles.Skill}>Skill Component</div>;
+}) => (
+	<div className={styles.Skill}>
+		<p>{skillName}</p>
+		{/* <div className={styles.progressBarTrack}>
+			<motion.div
+				className={styles.progressBarFill}
+				// Initial state (before animation)
+				initial={{ width: 0 }}
+				// Target state (where it should end up)
+				animate={{ width: `${loadingBarPercentage}%` }}
+				// "Physics" of the movement
+				transition={{ duration: 1.2, ease: "easeOut" }}
+			/>
+		</div>. I NEED TO GO THROUGH THIS AND FINE TUNE THIS, AS ITS AN EXTERNAL EXAMPLE */}
+	</div>
+);
 
 export default Skill;
